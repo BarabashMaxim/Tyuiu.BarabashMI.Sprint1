0923cc9a2651aa86459da75b95d0d74a4a8543e1
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-09-21 20:07:56 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.BarabashMI.Sprint1.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BarabashMI.Sprint1.Task0.V1.Lib;
+namespace Tyuiu.BarabashMI.Sprint1.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-6, res);
+        }
+    }
+}
