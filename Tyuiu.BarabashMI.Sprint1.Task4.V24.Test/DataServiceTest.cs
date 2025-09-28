49bf7725504b3bc7b9fb187dc9da20f7ba851984
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-09-28 16:34:36 +0500

    Task4

---
+++ Tyuiu.BarabashMI.Sprint1.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BarabashMI.Sprint1.Task4.V24.Lib;
+namespace Tyuiu.BarabashMI.Sprint1.Task4.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.287, res);
+        }
+    }
+}
