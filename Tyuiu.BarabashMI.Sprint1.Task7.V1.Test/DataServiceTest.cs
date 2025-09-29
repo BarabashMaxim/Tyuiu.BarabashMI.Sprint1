9f13507a0d819d1beefc6ff87153db4f5664a111
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-09-29 12:41:59 +0500

    Task7

---
+++ Tyuiu.BarabashMI.Sprint1.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BarabashMI.Sprint1.Task7.V1.Lib;
+namespace Tyuiu.BarabashMI.Sprint1.Task7.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double a = 1, b = 2, c = 3;
+            double res = 0.25;
+            Assert.AreEqual(res, ds.Calculate(a, b ,c));
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint1.Task7.V1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BarabashMI.Sprint1.Task7.V1.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил Барабаш М. И. | ИИПб-25-1";
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* Спринт #1                                                                *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                         *");
+        Console.WriteLine("* Задание #7                                                               *");
+        Console.WriteLine("* Вариант #1                                                               *");
+        Console.WriteLine("* Выполнил: Барабаш Максим Игоревич | ИИПб-25-1                            *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение           *");
+        Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                    *");
+        Console.WriteLine("*                                                                          *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+        Console.WriteLine("****************************************************************************");
+        double a, b, c;
+        Console.WriteLine("Введите число a:");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите число b:");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите число c:");
+        c = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+        Console.WriteLine("****************************************************************************");
+
+        Console.WriteLine(ds.Calculate(a, b, c));
+
+
+
+
+
+        Console.ReadKey();
+    }
+}
